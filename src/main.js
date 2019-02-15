40cf0b706aa20711b294047e140ab70b076ec7aa
Rubzaobrother <1589684519@qq.com>
2019-02-15 23:43:24 +0800

    缩略图完成

--- src/main.js
+++ src/main.js
@@ -35,6 +35,10 @@ Vue.filter('dateFormat', function(dataStr, pattern = 'YYYY-MM-DD HH:mm:ss'){
 import comment from './components/comment.vue'
 Vue.component('comment',comment)
 
+// 安装vue-pic-preview
+import VuePreview from 'vue-pic-preview'
+Vue.use(VuePreview)
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/pages/photoInfo/index.vue
+++ src/pages/photoInfo/index.vue
@@ -1,63 +1,89 @@
 <template>
-    <div class="photoinfo-container">
-        <h3>{{ photoInfo.title }}</h3>
-        <p class="subtitle">
-            <span>发表时间:{{ photoInfo.add_time | dateFormat}}</span>
-            <span>点击:{{ photoInfo.click }}次</span>
-        </p>
+  <div class="photoinfo-container">
+    <h3>{{ photoInfo.title }}</h3>
+    <p class="subtitle">
+      <span>发表时间:{{ photoInfo.add_time | dateFormat}}</span>
+      <span>点击:{{ photoInfo.click }}次</span>
+    </p>
 
-        <hr>
+    <hr>
 
-        <!-- 缩略图区域 -->
-
-        <!-- 图片内容区域 -->
-        <div class="content" v-html="photoInfo.content"></div>
-
-        <!-- 评论子组件 -->
-        <comment :id=id></comment>
+    <!-- 缩略图区域 -->
+    <div class="thumbs">
+       <img 
+       class="preview-img" 
+       v-for="(item, index) in list"
+       :key="index"
+       :src="item.src" 
+       height="100" 
+       @click="$preview.open(index, list)" />
     </div>
+
+    <!-- 图片内容区域 -->
+    <div class="content" v-html="photoInfo.content"></div>
+
+    <!-- 评论子组件 -->
+    <comment :id="id"></comment>
+  </div>
 </template>
 
 <script>
-
-
 export default {
-    created() {
-        this.getPhotoInfo()
+  created() {
+    this.getPhotoInfo();
+    this.getThumbs();
+  },
+  data() {
+    return {
+      id: this.$route.params.id,
+      photoInfo: {},
+      list: []
+    };
+  },
+  methods: {
+    getPhotoInfo() {
+      this.$http.get("getimageInfo/" + this.id).then(result => {
+        this.photoInfo = result.body.message[0];
+      });
     },
-    data(){
-        return {
-            id: this.$route.params.id,
-            photoInfo:{}
+    getThumbs() {
+      this.$http.get("getthumimages/" + this.id).then(result => {
+        if (result.body.status === 0) {
+          result.body.message.forEach(item => {
+            item.w = 600;
+            item.h = 400;
+          });
         }
-    },
-    methods: {
-        getPhotoInfo(){
-            this.$http.get("getimageInfo/" + this.id).then(result =>{
-                this.photoInfo = result.body.message[0]
-            })
-        }
-    },
-}
+        this.list = result.body.message;
+      });
+    }
+  }
+};
 </script>
 
 <style lang="less">
-    .photoinfo-container{
-        padding: 3px;
-        h3{
-            color: #26A2FF;
-            font-size: 15px;
-            text-align: center;
-            margin: 15px 0;
-        }
-        .subtitle{
-            display: flex;
-            justify-content: space-between; 
-            font-size: 13px;
-        }
-        .content{
-            font-size: 13px;
-            line-height: 30px;
-        }
+.photoinfo-container {
+  padding: 3px;
+  h3 {
+    color: #26a2ff;
+    font-size: 15px;
+    text-align: center;
+    margin: 15px 0;
+  }
+  .subtitle {
+    display: flex;
+    justify-content: space-between;
+    font-size: 13px;
+  }
+  .content {
+    font-size: 13px;
+    line-height: 30px;
+  }
+  .thumbs {
+    img {
+      margin: 4px;
+      box-shadow: 0 0 8px #999;
     }
+  }
+}
 </style>
